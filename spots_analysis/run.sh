48c27025ca7e4603a514b8e58dcc6da4edeff438
PatrickHFA <patrick.hideki.fuzimoto@gmail.com>
2024-10-18 12:08:07 -0300

    Submit to HPCC Marvin and time it execution

--- spots_analysis/run.sh
+++ spots_analysis/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=spots_cellprofiler_analysis
+#SBATCH --job-name=spots_analysis
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=short-gpu-small
@@ -9,10 +9,9 @@
 
 # Processing plate
 echo "[==> Processing plate";
-singularity run --nv /opt/images/cellprofiler/cellprofiler-4_2_6.sif -c -r -p proj.cpproj
+time singularity run --nv /opt/images/cellprofiler/cellprofiler-4_2_6.sif -c -r -p spots_analysis.cppipe -i data -o results --images-per-batch=12
  
 # Post-process plate
 echo "[==> Post-processing";
-python postprocess.py; 
-
+# python postprocess.py;
 
